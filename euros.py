cb7a77cd0fbad958e4ea5db44669c410334faf94
conradparkzes <conradnexus@gmail.com>
2024-06-27 02:41:50 +0000

    added comments

--- euros.py
+++ euros.py
@@ -7,17 +7,18 @@ list_of_matches = []
 
 
 def getMatches(stage, teamA, teamB):
-    url = "https://euro-20242.p.rapidapi.com/matches"
+    url_match_info = "https://euro-20242.p.rapidapi.com/matches"
 
     header = {
         "x-rapidapi-key": "462634edaamshbb062caa9f855bfp1d5ea4jsna71d5565c6f4",
         "x-rapidapi-host": "euro-20242.p.rapidapi.com"
     }
 
-    output = requests.get(url, headers=header)
+    output = requests.get(url_match_info, headers=header)
     all_teams = output.json()
 
     winner = None
+    # each match will get its own dictionary of data
     match_facts = {}
     for item in all_teams:
         if item['stage'] == stage:
@@ -33,17 +34,17 @@ def getMatches(stage, teamA, teamB):
                     match_facts['team_b_score'] = team_b_score
                     match_facts['winning team'] = winner
 
-    url_rankings =
-    "https://footapi7.p.rapidapi.com/api/rankings/uefa/countries"
+    url_rank = "https://footapi7.p.rapidapi.com/api/rankings/uefa/countries"
 
     headers = {
         "x-rapidapi-key": "f4dc7ad289mshc637ece0d7896b0p1f223fjsndd4ae4e349b8",
         "x-rapidapi-host": "footapi7.p.rapidapi.com"
     }
 
-    response = requests.get(url_rankings, headers=headers)
+    response = requests.get(url_rank, headers=headers)
     all_rankings = response.json()
-
+    
+    # FootAPI capitalizes first letter, so editing country name
     teamA_upper = teamA[0].upper() + teamA[1:]
     teamB_upper = teamB[0].upper() + teamB[1:]
 
@@ -52,7 +53,6 @@ def getMatches(stage, teamA, teamB):
 
     # iterate through the rankings to find the points for teamA and teamB
     for all_countries in all_rankings['rankings']:
-        # row_name = ranking["rowName"].upper()
         if all_countries['rowName'] == teamA_upper:
             points_teamA = all_countries["points"]
         elif all_countries['rowName'] == teamB_upper:
@@ -60,6 +60,7 @@ def getMatches(stage, teamA, teamB):
 
     exp_team_name = None
 
+    # figuring out expected game winner based on points ranking
     if points_teamA > points_teamB:
         exp_team_name = teamA
     elif points_teamB > points_teamA:
@@ -70,6 +71,7 @@ def getMatches(stage, teamA, teamB):
     more_points = max(points_teamA, points_teamB)
     less_points = min(points_teamA, points_teamB)
 
+    # ratio of goals for expected winning team
     exp = int(more_points//less_points)
 
     if exp_team_name == "Draw":
@@ -84,11 +86,13 @@ def getMatches(stage, teamA, teamB):
 getMatches('groupStage', 'belgium', 'slovakia')
 getMatches('groupStage', 'germany', 'scotland')
 
+# hnndling creating the SQL table
 dataf = pd.DataFrame(list_of_matches)
-
 engine = db.create_engine('sqlite:///euros2024.db')
 dataf.to_sql('all_matches', con=engine, if_exists='replace', index=False)
 with engine.connect() as connection:
-    query_result =
-    connection.execute(db.text("SELECT * FROM all_matches;")).fetchall()
+    query_result = connection.execute(
+    db.text("SELECT * FROM all_matches;")
+).fetchall()
+
     print(pd.DataFrame(query_result))
